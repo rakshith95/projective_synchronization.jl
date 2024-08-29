efb8367ce80530ca6761d4a88db21b586897868d
Rakshith <madharak@fel.cvut.cz>
2024-08-29 13:43:47 +0200

    Changed Huber loss back

--- src/common/losses.jl
+++ src/common/losses.jl
@@ -4,7 +4,7 @@ const c_huber=1.345
 const c_welsch=2.985
 const c_fair = 1.4
 
-huber(r) = 1 / max(1e-3, abs(r)) 
+huber(r) = 1 / max(1.0, abs(r)) 
 cauchy(r) = 1 / (1 + r^2)
 welsch(r) = exp(-(r^2))
 fair(r) = 1 / (1 + abs(r))
